fa31059489db1afa4de1c24ff09d06a1f14fa6fc
Evgeny Ryzhkov <ryzhkov_ev@inbox.ru>
2026-09-06 09:35:00 +0300

    Initial commit: Otus project

---
+++ src/test/java/com/rev/otus/OtusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rev.otus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
